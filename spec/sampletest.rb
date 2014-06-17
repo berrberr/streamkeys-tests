e7554a4dabe8c128c575d99a1b129a80f0634b9e
Alex Gabriel <alexg@criticalmass.com>
2014-06-17 16:57:40 -0400

    First attempts to send keys.

--- spec/sampletest.rb
+++ spec/sampletest.rb
@@ -1,7 +1,7 @@
 require 'selenium-webdriver'
 require 'rspec'
 
-describe "Rspec-example" do
+describe "Grooveshark suite" do
   before(:all) do
     caps = Selenium::WebDriver::Remote::Capabilities.chrome(
       "chromeOptions" => {
@@ -12,17 +12,19 @@ describe "Rspec-example" do
       }, "loggingPrefs" => {"browser" => "ALL"})
     #-2147483648
     #.manage.logs.get("browser")
-    @driver = Selenium::WebDriver.for :chrome, desired_capabilities: caps
+    @driver = Selenium::WebDriver.for :remote, url: "http://127.0.0.1:4444", desired_capabilities: caps
     @driver.navigate.to "http://www.grooveshark.com"
-    @wait =  Selenium::WebDriver::Wait.new(:timeout => 10)
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
   end
 
   after(:all) do
+    puts @driver.manage.logs.get("browser")
+    puts @driver.manage.logs.get("driver")
     @driver.quit
   end
 
   it "Verify grooveshark loads" do
-    expect(@driver.find_element(:name,"q").enabled?).to be(true)
+    #expect(@driver.find_element(:name, "q").enabled?).to be(true)
     #@driver.find_element(:name,"q").
     #Wait until  'flight finder' appears
     #@wait.until {@driver.find_element(:xpath,"/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr/td/img")}
@@ -31,7 +33,34 @@ describe "Rspec-example" do
   end
 
   it "Verify StreamKeys loads" do
+    puts "is true true?"
     expect(true).to be true
   end
 
+  it "Verify play works" do
+    sleep 1
+    @driver.find_element(:css, "*")
+
+    @driver.action.key_down(:command).key_down(:shift).send_keys(2).perform
+    sleep 1
+
+    @driver.action.key_down(:command).key_down(:shift).send_keys("2").perform
+
+    # @driver.action.send_keys [:command, :shift, "3"]
+    # sleep 2
+    # @driver.action.send_keys :command, :shift, "2"
+    # @driver.action.send_keys :command, :shift, 1
+  end
+
+  # it "Verify pause works" do
+  # end
+
+  # it "Verify next track works" do
+  # end
+
+  # it "Verify previous track works" do
+  # end
+
+  # it "Verify mute works" do
+  # end
 end
